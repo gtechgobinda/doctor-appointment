06c7cef18234fd09d98f0fc69719766e52e90d37
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2024-11-23 11:54:03 +0530

    initial project setup

---
+++ src/components/PatientHomePage/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PatientHome = () => {
+  return (
+    <>
+      <p>Patient Home page</p>
+    </>
+  );
+};
+
+export default PatientHome;
---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDY-mHNGgD_1xUohhfbXI_UDmq0WM7pMp4",
+  authDomain: "doctor-appointment-e481a.firebaseapp.com",
+  projectId: "doctor-appointment-e481a",
+  storageBucket: "doctor-appointment-e481a.firebasestorage.app",
+  messagingSenderId: "292104215326",
+  appId: "1:292104215326:web:dd26c7c0af56511dd8470f",
+};
+
+const app = initializeApp(firebaseConfig);
